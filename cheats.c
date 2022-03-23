3a772aae1d183bb7151251d299c646b67de02c0e
KingCraXy <cobergage@gmail.com>
2022-03-22 22:26:01 -0500

    Add files via upload

---
+++ cheats.c
@@ -0,0 +1,22 @@
+﻿void	Rainbow_Clothes_vTwo(void)
+{
+	memcpy((void *)(1482FFC), (void*)(offset)), 0x00000020);
+	if (READU32(offset + 0x0482FFC) == 0x0888D818)
+	{
+		WRITEU32(offset + 0x0482FFC, 0x0888D638);
+	}
+	memcpy((void *)(1483000), (void*)(offset)), 0x00000020);
+	if (READU32(offset + 0x0483000) == 0x0888D818)
+	{
+		WRITEU32(offset + 0x0483000, 0x0888D638);
+	}
+	memcpy((void *)(1483004), (void*)(offset)), 0x00000020);
+	if (READU32(offset + 0x0483004) == 0x0888D818)
+	{
+		WRITEU32(offset + 0x0483004, 0x0888D638);
+		offset = 0;
+		data = 0;
+	}
+}
+
+
